679fa96cb6217f7df45bcd183f661658b97e8e67
David秦 <6576180+mengqin316@user.noreply.gitee.com>
2023-12-13 18:31:50 +0800

    initializing

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app>
+        <SideBar @selectPages=selectPages></SideBar>
+        <v-main>
+            <chooseClass v-if="pageSelected==0"></chooseClass>
+            <delClass v-if="pageSelected==1"></delClass>
+            <queryClass v-if="pageSelected==2"></queryClass>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SideBar from './components/SideBar.vue';
+import chooseClass from './components/chooseClass.vue';
+import delClass from './components/delClass.vue';
+import queryClass from './components/queryClass.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+
+    components: {
+        SideBar,
+        chooseClass,
+        delClass,
+        queryClass
+    },
+
+    data() {
+        return {
+            pageSelected: 0
+        };
+    },
+    methods: {
+        selectPages(pageidx) {
+            console.log(pageidx)
+            this.pageSelected = pageidx
+            console.log(this.pageSelected)
+        }
+
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<!-- 
+<script>
+    export default{
+        data(){
+            return {
+                msg:'Hello组件'
+            }
+        },
+        methods:{
+            changeMsg(){
+               return this.msg+='a'
+            }
+        },
+    }
+</script> -->
